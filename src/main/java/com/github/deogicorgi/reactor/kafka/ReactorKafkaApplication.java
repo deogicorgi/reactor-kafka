71b48a30a1b0da83b4e8992dd2f3df051b42dd92
deogicorgi <deogicorgi@gmail.com>
2022-04-01 19:46:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/github/deogicorgi/reactor/kafka/ReactorKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.deogicorgi.reactor.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorKafkaApplication.class, args);
+    }
+
+}
