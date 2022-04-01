95104fad1edbc9aa109ade31bf9194d9c840155a
deogicorgi <deogicorgi@gmail.com>
2022-04-01 20:29:38 +0900

    카프카 설정 추가

--- src/main/java/com/github/deogicorgi/reactor/kafka/producer/message/AbstractKafkaProduceMessage.java
+++ src/main/java/com/github/deogicorgi/reactor/kafka/producer/message/AbstractKafkaProduceMessage.java
@@ -1,19 +1,7 @@
 package com.github.deogicorgi.reactor.kafka.producer.message;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.deogicorgi.reactor.kafka.exception.ProducerServiceException;
-
 public abstract class AbstractKafkaProduceMessage {
 
     protected String message;
 
-    public void setMessage(Object message) throws ProducerServiceException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            this.message = objectMapper.writeValueAsString(message);
-        } catch (Exception e) {
-            throw new ProducerServiceException(e);
-        }
-
-    }
 }
