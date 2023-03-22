8702a9009cb3003c369a793342f8ad49b3c7f4bb
Deogicorgi <deogicorgi@gmail.com>
2023-03-23 06:41:23 +0900

    테스트 커밋
    테스트커밋

--- producer/src/main/java/com/github/deogicorgi/reactive/producer/ProducerApplication.java
+++ producer/src/main/java/com/github/deogicorgi/reactive/producer/ProducerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProducerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class, args);
     }
